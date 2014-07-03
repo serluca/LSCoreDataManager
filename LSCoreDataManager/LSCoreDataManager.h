322523f0b335a8cdc222866fc8ae3664c5fc7e22
Luca Serpico <serpicoluca@gmail.com>
2014-07-03 23:20:16 +0100

    Revert "Closed issue #1 #2 #3 #4"
    This reverts commit 683897d70061562f55a4f88424d376b99d616876.

--- LSCoreDataManager/LSCoreDataManager.h
+++ LSCoreDataManager/LSCoreDataManager.h
@@ -7,7 +7,6 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <CoreData/CoreData.h>
 
 @interface LSCoreDataManager : NSObject
 
@@ -15,11 +14,6 @@
  *  The main context
  */
 @property (strong, nonatomic, readonly) NSManagedObjectContext *mainObjectContext;
-
-/**
- *  The object Model
- */
-@property (strong, nonatomic, readonly) NSManagedObjectModel *managedObjectModel;
 
 +(LSCoreDataManager*)sharedInstance;
 
@@ -52,35 +46,5 @@
  *  Force saving to disk, this method is invoked automatically by the system
  */
 - (void)writeToDisk;
-
-/**
- *  Create a new NSFetchedResultsController from fetch request in your model file
- *
- *  @param templateName    The name of the template
- *  @param valueDictionary The dictionary with a values to send to model
- *  @param sortdescriptors An array with sort descriptors
- *  @param sectionKeyPath  The section key path
- *  @param cacheName       The cache name
- *
- *  @return A NSFetchedResultsController created
- */
--(NSFetchedResultsController *)newFetchedResultsControllerFromTemplate:(NSString *)templateName
-                                                                values:(NSDictionary *)valueDictionary
-                                                       sortDescriptors:(NSArray *)sortdescriptors
-                                                        sectionKeyPath:(NSString *)sectionKeyPath
-                                                          andCacheName:(NSString *)cacheName;
-
-/**
- *  Retrieve a fetch request from fetch requests in the model file
- *
- *  @param templateName    The name of the template
- *  @param valueDictionary The dictionary with a values to send to model
- *  @param sortdescriptors An array with sort descriptors
- *
- *  @return A fetch request retrieved
- */
--(NSFetchRequest *)newFetchRequestFromTemplate:(NSString *)templateName
-                                    withValues:(NSDictionary *)valueDictionary
-                            andSortDescriptors:(NSArray *)sortdescriptors;
 
 @end
--- LSCoreDataManager/LSCoreDataManager.m
+++ LSCoreDataManager/LSCoreDataManager.m
@@ -7,12 +7,12 @@
 //
 
 #import "LSCoreDataManager.h"
+#import <CoreData/CoreData.h>
 
 @interface LSCoreDataManager()
 
 @property (strong, nonatomic) NSManagedObjectContext       *privateWriterContext;
-@property (strong, nonatomic, readwrite) NSManagedObjectContext *mainObjectContext;
-@property (strong, nonatomic,readwrite) NSManagedObjectModel *managedObjectModel;
+@property (strong, nonatomic) NSManagedObjectModel         *managedObjectModel;
 @property (strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
 @property (strong, nonatomic) NSURL *modelURL;
@@ -69,12 +69,15 @@
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
     }
+    
+    // subscribe to change notifications
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_mocDidSaveNotification:) name:NSManagedObjectContextDidSaveNotification object:nil];
 }
 
 #pragma mark - Getter
 -(NSManagedObjectContext*)getNewContext{
     NSManagedObjectContext *temporaryContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
-    temporaryContext.parentContext = self.mainObjectContext;
+    temporaryContext.parentContext = _mainObjectContext;
     temporaryContext.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy;
     [temporaryContext setUndoManager:nil];
     return temporaryContext;
@@ -97,6 +100,23 @@
         _mainObjectContext.parentContext = self.privateWriterContext;
         return _mainObjectContext;
     }
+}
+
+#pragma mark - Notification 
+- (void)_mocDidSaveNotification:(NSNotification *)notification{
+    NSManagedObjectContext *savedContext = [notification object];
+    
+    // Ignore change notifications for the main MOC
+    if (_mainObjectContext == savedContext){
+        return;
+    }
+    
+    if (_mainObjectContext.persistentStoreCoordinator != savedContext.persistentStoreCoordinator){
+        // that's another database
+        return;
+    }
+    
+    [_mainObjectContext mergeChangesFromContextDidSaveNotification:notification];
 }
 
 #pragma mark - Public methods
@@ -142,35 +162,4 @@
         
     }];
 }
-
-#pragma mark - Fetch Request Template
-- (NSFetchRequest *)newFetchRequestFromTemplate:(NSString *)templateName
-                                     withValues:(NSDictionary *)valueDictionary
-                             andSortDescriptors:(NSArray *)sortdescriptors {
-    NSManagedObjectModel *model = self.managedObjectModel;
-    NSFetchRequest *fetchRequest = [model fetchRequestFromTemplateWithName:templateName substitutionVariables:valueDictionary];
-    fetchRequest.sortDescriptors = sortdescriptors;
-    
-    return fetchRequest;
-}
-
-- (NSFetchedResultsController *)newFetchedResultsControllerFromTemplate:(NSString *)templateName
-                                                                 values:(NSDictionary *)valueDictionary
-                                                        sortDescriptors:(NSArray *)sortdescriptors
-                                                         sectionKeyPath:(NSString *)sectionKeyPath
-                                                           andCacheName:(NSString *)cacheName {
-    
-    NSFetchRequest *fetchRequest = [self newFetchRequestFromTemplate:templateName
-                                                          withValues:valueDictionary
-                                                  andSortDescriptors:sortdescriptors];
-    
-    NSManagedObjectContext *context = self.mainObjectContext;
-    NSFetchedResultsController *fetchController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
-                                                                                      managedObjectContext:context
-                                                                                        sectionNameKeyPath:sectionKeyPath
-                                                                                                 cacheName:nil];
-    return fetchController;
-}
-
-
 @end
